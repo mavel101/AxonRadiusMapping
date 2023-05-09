8c21150c76e69dcf55eb2b853f642765f23e65eb
mavel101 <marten.veldmann@hhu.de>
2023-05-09 15:59:42 +0200

    allow some tolerance for corrected bvalues

--- correctgradient.m
+++ correctgradient.m
@@ -8,8 +8,9 @@ dims=dims(1:3);
 gd=reshape(gd,[dims,3,3]);
 gd=permute(gd,[5,4,1,2,3]);
 
-bidxs{1}=bval==bval1;
-bidxs{2}=bval==bval2;
+% allow some tolerance
+bidxs{1}=(bval<bval1+100 & bval>bval1-100);
+bidxs{2}=(bval<bval2+100 & bval>bval2-100);
 
 corr_fac=cell(2,1);
 for bidx=1:length(bidxs)
